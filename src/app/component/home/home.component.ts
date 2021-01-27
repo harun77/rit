ce614a802458912efc33f964687924c98ff87e0a
Harun Or Rashid <hor@bordingvista.com.bd.local>
2021-01-27 12:50:25 +0600

    Added home carousel images

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  images = [
+    '/assets/images/home/1.jpg',
+    '/assets/images/home/2.jpg',
+    '/assets/images/home/3.jpg',
+    '/assets/images/home/4.png',
+    '/assets/images/home/5.jpg',
+  ];
+
   constructor() { }
 
   ngOnInit(): void { }
