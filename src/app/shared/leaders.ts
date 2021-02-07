52366c906054085e9128e1df0f40819c64ada5ea
Harun Or Rashid <hor@bordingvista.com.bd.local>
2021-02-07 13:35:02 +0600

    Code improvement

--- src/app/shared/leaders.ts
+++ src/app/shared/leaders.ts
@@ -2,7 +2,7 @@ import { Leader } from './leader';
 
 export const LEADERS: Leader[] = [
     {
-        id: '0',
+        id: '1',
         name: 'A.K.M. Harun Or Rashid',
         image: '/assets/images/harun.jpg',
         designation: 'Director & CEO',
@@ -10,133 +10,25 @@ export const LEADERS: Leader[] = [
         linkedin: 'https://www.linkedin.com/in/harunur-rashid-36a279a6/'
     },
     {
-        id: '1',
+        id: '2',
         name: 'MD. Al Masud Ali Khan',
         image: '/assets/images/masud.jpg',
         designation: 'MD'
-    }, {
-        id: '2',
+    },
+    {
+        id: '3',
         name: 'Golam Mosihuddin Sikder (Mohsin)',
         image: '/assets/images/mohsin.jpg',
         designation: 'Chairman'
-    }, {
-        id: '0',
+    },
+    {
+        id: '4',
         name: 'A.K.M. Harun Or Rashid',
-        designation: 'Founder & CEO'
-    }, {
-        id: '0',
+        designation: 'Director'
+    },
+    {
+        id: '5',
         name: 'A.K.M. Harun Or Rashid',
-        image: '/assets/images/harun.jpg',
-        designation: 'Founder & CEO'
-    }, {
-        id: '0',
-        name: 'A.K.M. Harun Or Rashid',
-        image: '/assets/images/harun.jpg',
-        designation: 'Founder & CEO'
-    }, {
-        id: '0',
-        name: 'A.K.M. Harun Or Rashid',
-        image: '/assets/images/harun.jpg',
-        designation: 'Founder & CEO'
-    }, {
-        id: '0',
-        name: 'A.K.M. Harun Or Rashid',
-        image: '/assets/images/harun.jpg',
-        designation: 'Founder & CEO'
-    }, {
-        id: '0',
-        name: 'A.K.M. Harun Or Rashid',
-        image: '/assets/images/harun.jpg',
-        designation: 'Founder & CEO'
-    }, {
-        id: '0',
-        name: 'A.K.M. Harun Or Rashid',
-        image: '/assets/images/harun.jpg',
-        designation: 'Founder & CEO'
-    }, {
-        id: '0',
-        name: 'A.K.M. Harun Or Rashid',
-        image: '/assets/images/harun.jpg',
-        designation: 'Founder & CEO'
-    }, {
-        id: '0',
-        name: 'A.K.M. Harun Or Rashid',
-        image: '/assets/images/harun.jpg',
-        designation: 'Founder & CEO'
-    }, {
-        id: '0',
-        name: 'A.K.M. Harun Or Rashid',
-        image: '/assets/images/harun.jpg',
-        designation: 'Founder & CEO'
-    }, {
-        id: '0',
-        name: 'A.K.M. Harun Or Rashid',
-        image: '/assets/images/harun.jpg',
-        designation: 'Founder & CEO'
-    }, {
-        id: '0',
-        name: 'A.K.M. Harun Or Rashid',
-        image: '/assets/images/harun.jpg',
-        designation: 'Founder & CEO'
-    }, {
-        id: '0',
-        name: 'A.K.M. Harun Or Rashid',
-        image: '/assets/images/harun.jpg',
-        designation: 'Founder & CEO'
-    }, {
-        id: '0',
-        name: 'A.K.M. Harun Or Rashid',
-        image: '/assets/images/harun.jpg',
-        designation: 'Founder & CEO'
-    }, {
-        id: '0',
-        name: 'A.K.M. Harun Or Rashid',
-        image: '/assets/images/harun.jpg',
-        designation: 'Founder & CEO'
-    }, {
-        id: '0',
-        name: 'A.K.M. Harun Or Rashid',
-        image: '/assets/images/harun.jpg',
-        designation: 'Founder & CEO'
-    }, {
-        id: '0',
-        name: 'A.K.M. Harun Or Rashid',
-        image: '/assets/images/harun.jpg',
-        designation: 'Founder & CEO'
-    }, {
-        id: '0',
-        name: 'A.K.M. Harun Or Rashid',
-        image: '/assets/images/harun.jpg',
-        designation: 'Founder & CEO'
-    }, {
-        id: '0',
-        name: 'A.K.M. Harun Or Rashid',
-        image: '/assets/images/harun.jpg',
-        designation: 'Founder & CEO'
-    }, {
-        id: '0',
-        name: 'A.K.M. Harun Or Rashid',
-        image: '/assets/images/harun.jpg',
-        designation: 'Founder & CEO'
-    }, {
-        id: '0',
-        name: 'A.K.M. Harun Or Rashid',
-        image: '/assets/images/harun.jpg',
-        designation: 'Founder & CEO'
-    }, {
-        id: '0',
-        name: 'A.K.M. Harun Or Rashid',
-        image: '/assets/images/harun.jpg',
-        designation: 'Founder & CEO'
-    }, {
-        id: '0',
-        name: 'A.K.M. Harun Or Rashid',
-        image: '/assets/images/harun.jpg',
-        designation: 'Founder & CEO'
-    }, {
-        id: '0',
-        name: 'A.K.M. Harun Or Rashid',
-        image: '/assets/images/harun.jpg',
-        designation: 'Founder & CEO'
+        designation: 'Director'
     }
 ];
