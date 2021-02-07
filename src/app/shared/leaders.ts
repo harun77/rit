6c5b82f8dc5e94bec5dce72ab387862ccb648564
Harun Or Rashid <hor@bordingvista.com.bd.local>
2021-02-07 21:07:11 +0600

    Code improvement

--- src/app/shared/leaders.ts
+++ src/app/shared/leaders.ts
@@ -4,21 +4,21 @@ export const LEADERS: Leader[] = [
     {
         id: '1',
         name: 'A.K.M. Harun Or Rashid',
-        image: '/assets/images/harun.jpg',
+        image: '/assets/images/leaders/me.jpg',
         designation: 'Director & CEO',
         facebook: 'https://www.facebook.com/ghumpok/',
         linkedin: 'https://www.linkedin.com/in/harunur-rashid-36a279a6/'
     },
     {
         id: '2',
-        name: 'MD. Al Masud Ali Khan',
-        image: '/assets/images/masud.jpg',
-        designation: 'MD'
+        name: 'Golam Mosihuddin Sikder (Mohsin)',
+        image: '/assets/images/leaders/mohsin.jpg',
+        designation: 'Managing Director'
     },
     {
         id: '3',
-        name: 'Golam Mosihuddin Sikder (Mohsin)',
-        image: '/assets/images/mohsin.jpg',
+        name: 'MD. Al Masud Ali Khan',
+        image: '/assets/images/leaders/masud.jpg',
         designation: 'Chairman'
     }
 ];
